5a5a81ee992718464d882070b8cb8f05731ecd67
Omar <darknoon123@gmail.com>
2024-10-28 20:22:29 +0000

    update test_stack

--- Actividades/aserciones_pruebas/test_stack.py
+++ Actividades/aserciones_pruebas/test_stack.py
@@ -1,35 +1,3 @@
-"""
-from unittest import TestCase
-from stack import Stack
-
-class TestStack(TestCase):
-    """Casos de prueba para la Pila"""
-
-    def setUp(self):
-        """Configuración antes de cada prueba"""
-        self.stack = Stack()
-
-    def tearDown(self):
-        """Limpieza después de cada prueba"""
-        self.stack = None
-
-    def test_push(self):
-        """Prueba de insertar un elemento en la pila"""
-        raise Exception("no implementado")
-
-    def test_pop(self):
-        """Prueba de eliminar un elemento de la pila"""
-        raise Exception("no implementado")
-
-    def test_peek(self):
-        """Prueba de observar el elemento superior de la pila"""
-        raise Exception("no implementado")
-
-    def test_is_empty(self):
-        """Prueba de si la pila está vacía"""
-        raise Exception("no implementado")
-"""
-
 from unittest import TestCase
 from stack import Stack
 
